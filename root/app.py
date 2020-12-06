2701dcbfff84a2bc8836a779987380e407eb1302
juehuil <juehuil@uci.edu>
2020-12-07 00:09:50 +0800

    changed incorrect prod&dev to correct dev& prod

--- root/app.py
+++ root/app.py
@@ -5,9 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-ENV = 'dev'
+ENV = 'prod'
 
-if ENV == 'prod':
+if ENV == 'dev':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:lL@1998623@localhost/Test'
 else:
