2d38aa22764c84da76877cbf0f3b77a7ce147f24
juehuil <juehuil@uci.edu>
2021-01-03 23:48:33 +0800

    Added Music, User, UserExperience, UserMusic, UserMemory Tables.

--- root/app.py
+++ root/app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-ENV = 'prod'
+ENV = 'post'
 
 if ENV == 'dev':
     app.debug = True
@@ -18,19 +18,88 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class Feedback(db.Model):
-    __tablename__ = 'feedback'
-    id = db.Column(db.Integer, primary_key=True)
-    customer = db.Column(db.String(200), unique=True)
-    dealer = db.Column(db.String(200))
-    rating = db.Column(db.Integer)
-    comments = db.Column(db.Text())
 
-    def __init__(self, customer, dealer, rating, comments):
-        self.customer = customer
-        self.dealer = dealer
-        self.rating = rating
-        self.comments = comments
+class Music(db.Model):
+    __tablename__ = 'music'
+    mid = db.Column(db.Integer, primary_key=True)
+    mname = db.Column(db.String(200), unique=True)
+    murl = db.Column(db.String(200))
+    mtype = db.Column(db.Integer)   # 1: Classical, 2: Pop, 3: Yanni
+    mv = db.Column(db.Integer)
+    ma = db.Column(db.Integer)
+
+    def __init__(self, mid, mname, murl, mtype, mv, ma):
+        self.mid = mid
+        self.mname = mname
+        self.murl = murl
+        self.mtype = mtype
+        self.mv = mv
+        self.ma = ma
+class User(db.Model):
+    __tablename__ = 'user'
+    uid = db.Column(db.Integer, primary_key=True)
+    uname = db.Column(db.String(200), unique=True)
+    upw = db.Column(db.String(200))
+    utype = db.Column(db.Integer)       # 0: non, 0x100: Classical Fan, 0x010: pop Fan, 0x001: Yanni Fan
+
+    def __init__(self, uid, uname, upw, utype):
+        self.uid = uid
+        self.uname = uname
+        self.upw = upw
+        self.utype = utype
+class UserExp(db.Model):
+    __tablename__ = 'user_exp'
+    uid = db.Column(db.Integer, primary_key=True)
+    exp_num = db.Column(db.Integer, unique = True)
+    initial_v = db.Column(db.Integer)
+    initial_a = db.Column(db.Integer)
+    final_v = db.Column(db.Integer)
+    final_a = db.Column(db.Integer)
+    eval = db.Column(db.Integer)    ########
+    recommend_rate = db.Column(db.Integer)
+
+    def __init__(self, uid, exp_num, initial_v, initial_a, final_v, final_a, eval, recommend_rate):
+        self.uid = uid
+        self.exp_num =exp_num
+        self.initial_v = initial_v
+        self.initial_a = initial_a
+        self.final_v =final_v
+        self.final_a = final_a
+        self.eval = eval
+        self.recommend_rate = recommend_rate
+class UserMusic(db.Model):
+    __tablename__ = 'user_music'
+    uid = db.Column(db.Integer, primary_key=True)
+    exp_num = db.Column(db.Integer, primary_key=True, unique=True)
+    music_num = db.Column(db.Integer)
+    mid = db.Column(db.Integer)
+    v = db.Column(db.Integer)
+    a = db.Column(db.Integer)
+    score = db.Column(db.Integer)
+    familiarity = db.Column(db.Integer)
+
+    def __init__(self, uid, exp_num, music_num, mid, v, a, score, familiarity):
+        self.uid = uid
+        self.exp_num = exp_num
+        self.music_num = music_num
+        self.mid = mid
+        self.v = v
+        self.a = a
+        self.score = score
+        self.familiarity = familiarity
+class UserMemory(db.Model):
+    __tablename__ = 'user_memory'
+    uid = db.Column(db.Integer, primary_key=True)
+    exp_num = db.Column(db.Integer, primary_key=True, unique=True)
+    music_num = db.Column(db.Integer)
+    memory = db.Column(db.Text())
+
+    def __init__(self, uid, exp_num, music_num, memory):
+        self.uid = uid
+        self.exp_num = exp_num
+        self.music_num = music_num
+        self.memory = memory
+
 
 @app.route('/')
 def index():
