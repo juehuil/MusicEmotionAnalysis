a6c8900bee0e82f38ab8e5fad8e47499c9024db6
juehuil <juehuil@uci.edu>
2021-01-07 19:00:25 +0800

    Added pos neg conf and sent in usermemory table.

--- root/app.py
+++ root/app.py
@@ -206,11 +206,15 @@ def update_memory():
     user_exp_num = data["exp_num"]
     user_music_num = data["music_num"]
     user_mem = data["memory"]
-    result = str(get_sentiment_result(user_mem)['items'])
-    return result
-    new_memory = UserMemory(uid=user_id, exp_num=user_exp_num, music_num=user_music_num, memory=result)
+    pos = str(get_sentiment_result(user_mem)['items']['positive_prob'])
+    neg = str(get_sentiment_result(user_mem)['items']['negative_prob'])
+    conf = str(get_sentiment_result(user_mem)['items']['confidence'])
+    sent = str(get_sentiment_result(user_mem)['items']['sentiment'])
+
+    new_memory = UserMemory(uid=user_id, exp_num=user_exp_num, music_num=user_music_num, memory=user_mem, positive=pos, negative=neg, confidence=conf, sentiment=sent)
     db.session.add(new_memory)
     db.session.commit()
+    return user_mem
 
 def add_music(name, url, type, v, a):
     new_music = Music(mname=name, murl=url, mtype=type)
