142d78fd65574a3e5859e01cadf4a7e1320bf4b8
juehuil <juehuil@uci.edu>
2021-01-11 01:00:38 +0800

    Test Login.

--- root/app.py
+++ root/app.py
@@ -171,7 +171,7 @@ def login():
         if user_exp_num is None:
             return json.dumps({"u_id": str(user.uid), "exp_num": 0, "music_num": 0, "start_date": str(user.ustart)})
         # return str(user_exp_num.exp_num) + " " + str(user.ustart)
-        if user.exp_end is None:
+        if user_exp_num.exp_end is None:
             #user_music_num = UserMusic.query.filter_by(uid=user.uid, exp_num=user_exp_num.exp_num).order_by(UserMusic.music_num.desc()).first()
             #exp_past_time = datetime.datetime.now()-user_exp_num.exp_start
             #duration = int(exp_past_time.total_seconds()/3600)
